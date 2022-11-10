30ddf16e810e9204e4998773844360235d400328
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-10 12:16:10 +0530

    feat : Routes with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+// import components
+import Header from "./components/Header";
+import AnimRoutes from "./components/AnimRoutes";
+// import router
+import { BrowserRouter as Router } from "react-router-dom";
+
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-     Hello
-    </div>
+    <>
+      <Router>
+        <Header />
+        <AnimRoutes />
+      </Router>
+    </>
   );
 }
 
--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+
+// import pages
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Portfolio from "../pages/Portfolio";
+import Contact from "../pages/Contact";
+
+// import routes route & useLocation hook
+import { Routes, Route } from "react-router-dom";
 
 const AnimRoutes = () => {
-  return <div>AnimRoutes</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 };
 
 export default AnimRoutes;
