59db54927bcd66e44080f7dff28efdcf6e950b4a
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-10 13:14:11 +0530

    feat : socials

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
+    <header className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
       <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
         {/* logo */}
         <Link to={"/"} className="max-w-[200px]">
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,44 @@
 import React from "react";
-
+// import icons
+import {
+  ImFacebook,
+  ImTwitter,
+  ImPinterest,
+  ImInstagram,
+  ImYoutube,
+} from "react-icons/im";
 const Socials = () => {
-  return <div className="hidden xl:flex ml-24">Socials</div>;
+  return (
+    <div className="hidden xl:flex ml-24">
+      <ul className="flex gap-x-4">
+        <li>
+          <a href="http://wwww.facebook.com" target="_blank" rel="noreferrer">
+            <ImFacebook />
+          </a>
+        </li>
+        <li>
+          <a href="http://www.twitter.com" target="_blank" rel="noreferrer">
+            <ImTwitter />
+          </a>
+        </li>
+        <li>
+          <a href="http://www.pinterest.com" target="_blank" rel="noreferrer">
+            <ImPinterest />
+          </a>
+        </li>
+        <li>
+          <a href="http://www.instagram.com" target="_blank" rel="noreferrer">
+            <ImInstagram />
+          </a>
+        </li>
+        <li>
+          <a href="http://www.youtube.com" target="_blank" rel="noreferrer">
+            <ImYoutube />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
