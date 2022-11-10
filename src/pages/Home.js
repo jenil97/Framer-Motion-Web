cbdb534b42e23399ed4769fa5fa1528099a7fd16
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-10 18:37:52 +0530

    feat : Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,46 @@
 import React from "react";
-
+// import images
+import WomanImg from "../assets/home/woman.png";
+// import link
+import { Link } from "react-router-dom";
+// import motion
+import { motion } from "framer-motion";
+// import transition
+import { transition1 } from "../transitions";
+// cursor context
+import { CursorContext } from "../context/CursorContext";
 const Home = () => {
-  return <div className="section bg-blue-200">Home</div>;
+  return (
+    <div className="section">
+      <div className="container mx-auto h-full relative">
+        {/* text & img wrapper */}
+        <div className="flex flex-col justify-center">
+          {/* text */}
+          <div className="w-full lg:mt-36 pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+            <h1 className="h1 cursor-default">
+              photographer <br /> & film maker
+            </h1>
+            <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12 cursor-default">
+              Los Angeles, USA
+            </p>
+            <Link to={"/contact"} className="btn mb-[30px] lg:-mt-4 mt-4">
+              hire me
+            </Link>
+          </div>
+          {/* image */}
+          <div className="flex justify-end">
+            <div>
+              <img
+                src={WomanImg}
+                alt=""
+                className="lg:w-[530px] lg:h-[650px] lg:mr-[170px]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
