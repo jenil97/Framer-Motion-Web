3957bc247d8c417c1017d964992112bb7b6b9e7a
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-11 12:24:32 +0530

    fix :  About

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,10 +2,18 @@ import React from "react";
 
 import Women from "../assets/about/woman.png";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
 const About = () => {
   return (
-    <div className="section">
+    <motion.div
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section"
+    >
       <div className="container mx-auto h-full relative">
         {/* text & img wrapper */}
         <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
@@ -13,7 +21,13 @@ const About = () => {
             <img src={Women} alt="" />
           </div>
           {/* text */}
-          <div className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start">
+          <motion.div
+            initial={{ opacity: 0, y: "80%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "80%" }}
+            transition={transition1}
+            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
+          >
             <h1 className="h1 cursor-default">About me</h1>
             <p className="mb-12 max-w-sm cursor-default">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
@@ -27,10 +41,10 @@ const About = () => {
             <Link to={"/portfolio"} className="btn">
               View my work
             </Link>
-          </div>
+          </motion.div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
