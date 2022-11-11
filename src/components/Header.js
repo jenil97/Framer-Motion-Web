836c31bfa8a88e32ba0036ec2b6ad06ccf69d161
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-11 16:11:29 +0530

    fix : Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 
 // import components
 import Socials from "./Socials";
 import Logo from "../assets/header/logo.svg";
 import MobileNav from "./MobileNav";
-
-// import link
 import { Link } from "react-router-dom";
+import { CursorContext } from "../context/CursorContext";
 
 const Header = () => {
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
   return (
     <header className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
       <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
         {/* logo */}
-        <Link to={"/"} className="max-w-[200px]">
+        <Link
+          onMouseEnter={mouseEnterHandler}
+          onMouseLeave={mouseLeaveHandler}
+          to={"/"}
+          className="max-w-[200px]"
+        >
           <img src={Logo} alt="" />
         </Link>
 
         {/* nav - initially hidden - show on desktop mode */}
-        <nav className="hidden xl:flex gap-x-12 font-semibold">
+        <nav
+          onMouseEnter={mouseEnterHandler}
+          onMouseLeave={mouseLeaveHandler}
+          className="hidden xl:flex gap-x-12 font-semibold"
+        >
           <Link
             to={"/"}
             className="text-[#696c6d] hover:text-primary transition"
