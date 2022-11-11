ea5c32d249f03f40c8b1b8bb900f6a3411ccd637
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-11 11:56:50 +0530

    fix : AnimRoutes

--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -7,16 +7,21 @@ import Portfolio from "../pages/Portfolio";
 import Contact from "../pages/Contact";
 
 // import routes route & useLocation hook
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 const AnimRoutes = () => {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <AnimatePresence initial={true} mode="wait">
+      <Routes key={location.pathname} Location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
