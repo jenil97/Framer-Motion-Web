48d79ce1d3c30d68db8eb7b094c78c763744a5d4
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-11 12:45:39 +0530

    fix : Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,27 @@
 import React from "react";
 import woman from "../assets/contact/woman.png";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
 const Contact = () => {
   return (
-    <div className="section bg-white">
+    <motion.section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section bg-white"
+    >
       <div className="container mx-auto h-full">
         <div className="flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left">
           {/* bg */}
-          <div className="hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10">
-            bg
-          </div>
+          <motion.div
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={transition1}
+            className="hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10"
+          ></motion.div>
           {/* text & form */}
           <div className="lg:flex-1 lg:pt-12 px-4">
             <h1 className="h1 cursor-default">Contact me</h1>
@@ -41,12 +53,18 @@ const Contact = () => {
             </form>
           </div>
           {/* image */}
-          <div className="lg:flex-1">
+          <motion.div
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={{ transition: transition1, duration: 1.5 }}
+            className="lg:flex-1"
+          >
             <img src={woman} alt="" className="lg:w-[500px] h-[640px]" />
-          </div>
+          </motion.div>
         </div>
       </div>
-    </div>
+    </motion.section>
   );
 };
 
