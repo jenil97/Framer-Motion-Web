09d847d3f1d8335bd260c89316f8b386dec7bcc3
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-11 16:14:24 +0530

    fix : Socials

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,5 +1,4 @@
-import React from "react";
-// import icons
+import React, { useContext } from "react";
 import {
   ImFacebook,
   ImTwitter,
@@ -7,9 +6,17 @@ import {
   ImInstagram,
   ImYoutube,
 } from "react-icons/im";
+import { CursorContext } from "../context/CursorContext";
+
 const Socials = () => {
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
   return (
-    <div className="hidden xl:flex ml-24">
+    <div
+      onMouseEnter={mouseEnterHandler}
+      onMouseLeave={mouseLeaveHandler}
+      className="hidden xl:flex ml-24"
+    >
       <ul className="flex gap-x-4">
         <li>
           <a href="http://wwww.facebook.com" target="_blank" rel="noreferrer">
